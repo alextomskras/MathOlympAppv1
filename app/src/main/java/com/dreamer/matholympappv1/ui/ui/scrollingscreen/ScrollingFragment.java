ed845b77421a55669d16eddf9c1466095873c23f
fess <dreamer.alex.toms@gmail.com>
2022-12-23 13:09:17 +0300

    add if's in allert dialog for more variant of buttons in dialogs
    Signed-off-by: fess <dreamer.alex.toms@gmail.com>

--- app/src/main/java/com/dreamer/matholympappv1/ui/ui/scrollingscreen/ScrollingFragment.java
+++ app/src/main/java/com/dreamer/matholympappv1/ui/ui/scrollingscreen/ScrollingFragment.java
@@ -1,5 +1,6 @@
 package com.dreamer.matholympappv1.ui.ui.scrollingscreen;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -21,7 +22,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.dreamer.matholympappv1.R;
 import com.dreamer.matholympappv1.data.model.model.Zadachi;
 import com.dreamer.matholympappv1.databinding.FragmentScrollingBinding;
 import com.google.android.material.snackbar.Snackbar;
@@ -32,6 +36,7 @@ import java.util.List;
 public class ScrollingFragment extends Fragment {
 
     static final String TAG = "TAG";
+    NavController navController;
     MenuItem menuScroll;
     private @NonNull
     FragmentScrollingBinding binding;
@@ -43,6 +48,7 @@ public class ScrollingFragment extends Fragment {
     private String zadacha_hint;
     private String zadacha_solution;
     public AlertDialog.Builder builder;
+    private String MainMessage;
 
     public ScrollingFragment() {
 //        this.zadachiList = zadachiList;
@@ -54,23 +60,21 @@ public class ScrollingFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        //        zadachiList = (List<Zadachi>) zadachiList.get(1);
+        zadacha_main_body = "";
+        zadacha_answer = "";
+        zadacha_hint = "";
+        zadacha_solution = "";
+        MainMessage = "";
+        ;
+        intNavcontroller();
+//        getFragmentBundles();
     }
 
-    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
-//        menuInflater.inflate(R.menu.scroll_frag_menu, menu);
-        menuScroll = menu.add("hint");
-        menuScroll.setTitle("hint");
-//        menuScroll.setTitle(zadacha_hint);
-        menuScroll.setTitleCondensed("hint");
-//        menuScroll.setIcon(R.drawable.ic_baseline_alt_route_24);
-        menuScroll.setOnMenuItemClickListener(v ->
-                {
-                    Snackbar.make(getActivity().findViewById(android.R.id.content),
-                            zadacha_hint.toString(), Snackbar.LENGTH_LONG).show();
-                    return true;
-                }
-        );
+    private void getFragmentBundles() {
+
     }
+
 
 //    @Override
 //    public boolean onOptionsItemSelected(MenuItem item) {
@@ -105,12 +109,6 @@ public class ScrollingFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-//        zadachiList = (List<Zadachi>) zadachiList.get(1);
-        zadacha_main_body = "";
-        zadacha_answer = "";
-        zadacha_hint = "";
-        zadacha_solution = "";
-        ;
         Bundle bundle = getArguments();
         if (bundle != null) {
 
@@ -118,8 +116,9 @@ public class ScrollingFragment extends Fragment {
             zadacha_answer = bundle.getString("MyArgZadacha_answer");
             zadacha_hint = bundle.getString("MyArgZadacha_hint");
             zadacha_solution = bundle.getString("MyArgZadacha_solution");
+            Log.e(TAG, "iconImageViewOnClick at position8 " + bundle.getString("MyArgZadacha_main_body", "_PlaceHolder_"));
         }
-        Log.e(TAG, "iconImageViewOnClick at position8 " + bundle.getString("MyArgZadacha_main_body", "_PlaceHolder_"));
+
         binding = FragmentScrollingBinding.inflate(inflater, container, false);
         return binding.getRoot();
 //        return inflater.inflate(R.layout.fragment_scrolling, container, false);
@@ -143,6 +142,7 @@ public class ScrollingFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //Ищем все что есть на макете
+        NavController navController;
         final TextView zadachaTextView = binding.tvScrollMainText;
         final TextView answerTextView = binding.tvScrollAnswerText;
         final TextView hintTextView = binding.tvScrollHintText;
@@ -181,7 +181,7 @@ public class ScrollingFragment extends Fragment {
             @Override
             public void afterTextChanged(Editable s) {
                 String textEnter = edtxtAnswer.getText().toString();
-                checkAnswer(textEnter.toString());
+//                checkAnswer(textEnter.toString());
 //                registerViewModel.loginDataChanged(usernameEditText.getText().toString(),
 //                        passwordEditText.getText().toString());
             }
@@ -209,7 +209,7 @@ public class ScrollingFragment extends Fragment {
 
     private void checkAnswer(String answer) {
 
-        builder = new AlertDialog.Builder(getActivity());
+
 //        binding.content.saveButton.setOnClickListener(v -> {
         String etTexttitle = answer.toString();
         Log.d(TAG, "viewId: " + etTexttitle);
@@ -233,34 +233,97 @@ public class ScrollingFragment extends Fragment {
 
 
     private void alertDiaShow(String Title, String MainMessage) {
+        builder = new AlertDialog.Builder(getActivity());
 //        TextInputEditText etTitle = (TextInputEditText) findViewById(R.id.event_title_user_input_et);
 //        TextInputEditText etTime = (TextInputEditText) findViewById(R.id.event_time_user_input_et);
 //        TextInputEditText etNote = (TextInputEditText) findViewById(R.id.event_notes_user_input_et);
 //        String date = getCurrentLocalDateTimeStamp();
 //        Log.d(TAG,"____DATE= "+date);
-        builder
+        if (!Title.equals("") && Title.equals("USPEH")) {
+            builder
 //                .setMessage(getString(R.string.alertEvent_title)+etTitle.getText()+"\n"+getString(R.string.alertEvent_date)+date+"\n"+getString(R.string.alertEvent_time)+etTime.getText()+"\n"+getString(R.string.alertEvent_notes)+etNote.getText()+"\n")
-                .setMessage(MainMessage)
+                    .setMessage(MainMessage)
 //                .setMessage(R.string.alertEvent_date)
 //                .setMessage(R.string.alertEvent_time)
 //                .setMessage(R.string.alertEvent_notes)
-                .setTitle("REZULTAT " + Title)
-                .setCancelable(true)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
+                    .setTitle("REZULTAT " + Title)
+                    .setCancelable(true)
+                    .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
 //                        finish();
-                        Snackbar.make(getActivity().findViewById(android.R.id.content),
-                                "YESSSS", Snackbar.LENGTH_LONG).show();
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        //  Action for 'NO' Button
-                        dialog.cancel();
-                        Snackbar.make(getActivity().findViewById(android.R.id.content),
-                                "NOOOOOO", Snackbar.LENGTH_LONG).show();
-                    }
-                });
+//                        getActivity().getFragmentManager().popBackStack();
+                            dialog.cancel();
+                            navController.navigateUp();
+//                            alertDiaShow("HINT", "zadacha_hint " + zadacha_hint);
+                            Snackbar.make(getActivity().findViewById(android.R.id.content),
+                                    "YESSSS", Snackbar.LENGTH_LONG).show();
+                        }
+                    })
+//                    .setNeutralButton("exit", new DialogInterface.OnClickListener() {
+//                                public void onClick(DialogInterface dialog, int id) {
+//                                    //  Action for 'NO' Button
+//
+//                                    dialog.cancel();
+//                                    navController.navigateUp();
+//                                    Snackbar.make(getActivity().findViewById(android.R.id.content),
+//                                            "EXITTT", Snackbar.LENGTH_LONG).show();
+//                                }
+//                            }
+//                    )
+
+                    .setIcon(R.drawable.ic_baseline_bubble_chart_24)
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //  Action for 'NO' Button
+//                        navController.navigateUp();
+                            dialog.cancel();
+                            Snackbar.make(getActivity().findViewById(android.R.id.content),
+                                    "NOOOOOO", Snackbar.LENGTH_LONG).show();
+                        }
+                    });
+        } else {
+            builder
+//                .setMessage(getString(R.string.alertEvent_title)+etTitle.getText()+"\n"+getString(R.string.alertEvent_date)+date+"\n"+getString(R.string.alertEvent_time)+etTime.getText()+"\n"+getString(R.string.alertEvent_notes)+etNote.getText()+"\n")
+                    .setMessage(MainMessage)
+//                .setMessage(R.string.alertEvent_date)
+//                .setMessage(R.string.alertEvent_time)
+//                .setMessage(R.string.alertEvent_notes)
+                    .setTitle("REZULTAT " + Title)
+                    .setCancelable(true)
+                    .setPositiveButton("Hint", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+//                        finish();
+//                        getActivity().getFragmentManager().popBackStack();
+//                        navController.navigateUp();
+                            alertDiaShow("HINT", "zadacha_hint " + zadacha_hint);
+                            Snackbar.make(getActivity().findViewById(android.R.id.content),
+                                    "YESSSS", Snackbar.LENGTH_LONG).show();
+                        }
+                    })
+                    .setNeutralButton("exit", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    //  Action for 'NO' Button
+
+                                    dialog.cancel();
+                                    navController.navigateUp();
+                                    Snackbar.make(getActivity().findViewById(android.R.id.content),
+                                            "EXITTT", Snackbar.LENGTH_LONG).show();
+                                }
+                            }
+                    )
+
+                    .setIcon(R.drawable.ic_baseline_bubble_chart_24)
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //  Action for 'NO' Button
+//                        navController.navigateUp();
+                            dialog.cancel();
+                            Snackbar.make(getActivity().findViewById(android.R.id.content),
+                                    "NOOOOOO", Snackbar.LENGTH_LONG).show();
+                        }
+                    });
+
+        }
 
 //        Creating dialog box
 //        AlertDialog alert = builder.create();
@@ -271,4 +334,35 @@ public class ScrollingFragment extends Fragment {
 //    }
     }
 
+    private void intNavcontroller() {
+        Activity MainActivity = getActivity();
+        assert MainActivity != null;
+        navController = Navigation.findNavController(MainActivity, R.id.nav_host_fragment);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+//        menuInflater.inflate(R.menu.scroll_frag_menu, menu);
+//        getFragmentBundles();
+        menuScroll = menu.add("hint");
+        menuScroll.setTitle("hint");
+//        menuScroll.setTitle(zadacha_hint);
+        menuScroll.setTitleCondensed("hint");
+//        menuScroll.setIcon(R.drawable.ic_baseline_alt_route_24);
+        menuScroll.setOnMenuItemClickListener(v ->
+                {
+//                    getFragmentBundles();
+//                    alertDiaShow("HINT","zadacha_hint");
+                    Snackbar.make(getActivity().findViewById(android.R.id.content),
+                            zadacha_hint.toString(), Snackbar.LENGTH_LONG).show();
+                    return true;
+                }
+        );
+    }
 }
