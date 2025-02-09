6a839b892995e564aeae1a69f1e408402d22df4a
fess <dreamer.alex.toms@gmail.com>
2025-02-09 18:06:19 +0300

    add fb service fcm message toast
    Signed-off-by: fess <dreamer.alex.toms@gmail.com>

--- app/src/main/java/com/dreamer/matholympappv1/utils/LatestFirebaseMessagingService.java
+++ app/src/main/java/com/dreamer/matholympappv1/utils/LatestFirebaseMessagingService.java
@@ -1,7 +1,12 @@
 package com.dreamer.matholympappv1.utils;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.util.Log;
 
+import androidx.core.app.NotificationCompat;
+
+import com.dreamer.matholympappv1.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -21,5 +26,22 @@ public class LatestFirebaseMessagingService extends FirebaseMessagingService {
         super.onMessageReceived(remoteMessage);
         Log.d(TAG, "Message received: " + remoteMessage.getData());
         // Обработка входящего сообщения (например, уведомление)
+        Log.d(TAG, "Получено сообщение: " + remoteMessage.getData());
+
+        if (remoteMessage.getNotification() != null) {
+            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+    }
+
+    private void showNotification(String title, String message) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "FCM_CHANNEL")
+                .setSmallIcon(R.drawable.ic_baseline_bubble_chart_24)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
     }
 }
